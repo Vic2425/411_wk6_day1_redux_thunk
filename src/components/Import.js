ae9dedf3db8949e0e880fc329589d64ab2ac0ad7
Vic <vicjs612@gmail.com>
2023-04-11 20:42:22 -0500

    first commit

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,11 +1,90 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  Button,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  TableContainer,
+} from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
+const ITEM_HEIGHT = 52;
 const Import = (props) => {
-    // fill out this component
+  // fill out this component
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [idNum, setIdNum] = useState(null);
+  
+  const open = Boolean(anchorEl);
 
-    return (
-        <p>Import Component</p>
-    )
-}
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+    setIdNum(event.currentTarget.id);
+  };
 
-export default Import
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <TableContainer>
+      <p></p>
+      <Button variant="contained" color="primary" onClick={props.fetchMakes}>
+        Import
+      </Button>
+      <h2>COUNT: {props.makes.length}</h2>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Make</TableCell>
+            <TableCell>Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.makes.map((make, id) => (
+            <TableRow key={make.MakeId}>
+              <TableCell>{make.MakeId}</TableCell>
+              <TableCell>{make.MakeName}</TableCell>
+              <TableCell>
+                <IconButton
+                  aria-label="more"
+                  aria-controls="long-menu"
+                  aria-expanded={open ? "true" : undefined}
+                  aria-haspopup="true"
+                  onClick={handleClick}
+                  id={id}
+                >
+                  <MoreVertIcon />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMountedopen={Boolean(anchorEl)}
+        onClose={handleClose}
+        // PaperProps={{
+        //   style: {
+        //     maxHeight: ITEM_HEIGHT * 4.5,
+        //     width: 200,
+        //   },
+        // }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Button onClick={() => props.deleteMake(idNum)}>Delete</Button>
+        </MenuItem>
+      </Menu>
+    </TableContainer>
+  );
+};
+
+export default Import;
