a67b5452aa6307c2848981926d118ffa65e525aa
Vic <vicjs612@gmail.com>
2023-04-11 20:46:21 -0500

    Updates

---
+++ precache-manifest.8e0976524186c0f51105d33aa400127f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "035238e50945f65f6b331f242836565a",
+    "url": "/411_wk6_day1_redux_thunk/index.html"
+  },
+  {
+    "revision": "2d5265ca5788e624d246",
+    "url": "/411_wk6_day1_redux_thunk/static/css/main.be9515fe.chunk.css"
+  },
+  {
+    "revision": "c830f4b28fc0abef6416",
+    "url": "/411_wk6_day1_redux_thunk/static/js/2.7fd219f7.chunk.js"
+  },
+  {
+    "revision": "2d5265ca5788e624d246",
+    "url": "/411_wk6_day1_redux_thunk/static/js/main.f4fbf32b.chunk.js"
+  },
+  {
+    "revision": "fbf4cc4069f99ef900df",
+    "url": "/411_wk6_day1_redux_thunk/static/js/runtime~main.b32a8eb5.js"
+  }
+]);
