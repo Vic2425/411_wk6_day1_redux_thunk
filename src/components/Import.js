5b496662151ccd860e43c0cfd8609ffbc50b565e
Vic <vicjs612@gmail.com>
2023-04-13 18:42:55 -0500

    complete

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import {
   Button,
-  Container,
-  IconButton,
   Menu,
   MenuItem,
   Table,
@@ -26,6 +24,7 @@ const Import = (props) => {
     props.deleteMake(deleteId);
     setAnchorEl(null);
     setDeleteId(null);
+    console.log(deleteId);
   };
 
   const handleClick = (event, id) => {
@@ -55,9 +54,7 @@ const Import = (props) => {
               <TableCell>{make.MakeId}</TableCell>
               <TableCell>{make.MakeName}</TableCell>
               <TableCell>
-                <MoreVertIcon
-                  onClick={(event) => handleClick(event, make.MakeId)}
-                />
+                <MoreVertIcon onClick={(event) => handleClick(event, id)} />
               </TableCell>
             </TableRow>
           ))}
@@ -76,3 +73,57 @@ const Import = (props) => {
 };
 
 export default Import;
+
+// import React from "react";
+// import {
+//   Button,
+//   Container,
+//   IconButton,
+//   Menu,
+//   MenuItem,
+//   Table,
+//   TableHead,
+//   TableBody,
+//   TableRow,
+//   TableCell,
+// } from "@mui/material";
+// import DeleteIcon from "@mui/icons-material/Delete";
+// import MoreVert from "@mui/icons-material/MoreVert";
+
+// const Import = (props) => {
+//   // fill out this component
+
+//   return (
+//     <Container sx={{ mt: "15px", mx: "auto", width: "60%" }}>
+//       <Button variant="contained" color="primary" onClick={props.fetchMakes}>
+//         Import
+//       </Button>
+//       <h2>COUNT: {props.makes.length}</h2>
+//       <Table>
+//         <TableHead>
+//           <TableRow>
+//             <TableCell>Id</TableCell>
+//             <TableCell>Make</TableCell>
+//             <TableCell>Actions</TableCell>
+//           </TableRow>
+//         </TableHead>
+//         <TableBody>
+//           {props.makes.map((make, index) => (
+//             <TableRow key={make.MakeId}>
+//               <TableCell>{make["MakeId"]}</TableCell>
+//               <TableCell>{make["MakeName"]}</TableCell>
+//               <TableCell>
+//                 {/* <MoreVert></MoreVert> */}
+//                 <DeleteIcon
+//                   onClick={() => props.deleteMake(index)}
+//                   className="icon text-red"
+//                 />
+//                 {console.log(index)}
+//               </TableCell>
+//             </TableRow>
+//           ))}
+//         </TableBody>
+//       </Table>
+//     </Container>
+//   );
+// };
